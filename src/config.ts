83d2410ff951da2c0f6a738dce80db78ed943d26
AT13xe <845043294@qq.com>
2025-08-31 10:23:30 +0800

    更改随机图站点

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,7 @@ export const siteConfig: SiteConfig = {
 	},
 	background: {
 		enable: true, // Enable background image
-		src: "https://pic.atxe.qzz.io/h", // Background image URL (supports HTTPS)
+		src: "https://pic.at13xe.top/h", // Background image URL (supports HTTPS)
 		position: "center", // Background position: 'top', 'center', 'bottom'
 		size: "cover", // Background size: 'cover', 'contain', 'auto'
 		repeat: "no-repeat", // Background repeat: 'no-repeat', 'repeat', 'repeat-x', 'repeat-y'
@@ -62,7 +62,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "随机图",
-			url: "https://pic.atxe.qzz.io",
+			url: "https://pic.at13xe.top",
             external: true,
 		},
 		// {
