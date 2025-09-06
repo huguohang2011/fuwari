e0fe1651816b2fc2f2223dc3c1dbeb8de8787096
huguohang2011 <2232768028@qq.com>
2025-09-07 07:34:09 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.at13xe.top",
+	site: "https://cnm.at13xe.top",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
