6495fe4da9cd054cce00f50e559a9529126ddd68
huguohang2011 <2232768028@qq.com>
2025-09-07 09:30:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -21,8 +21,8 @@ export const siteConfig: SiteConfig = {
 		forceDarkMode: true, // Force dark mode and hide theme switcher
 	},
 	banner: {
-		enable: false,
-		src: "assets/hutao.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/9.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
@@ -33,7 +33,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	background: {
-		enable: true, // Enable background image
+		enable: false, // Enable background image
 		src: "assets/9.jpg", // Background image URL (supports HTTPS)
 		position: "center", // Background position: 'top', 'center', 'bottom'
 		size: "cover", // Background size: 'cover', 'contain', 'auto'
